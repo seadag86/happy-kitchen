76fdb50f09fda540a4f3386bcf2a2d914b64d796
Myles Gregory <hello@mylesgregory.me>
2019-09-06 14:52:20 -0700

    Add Search component

---
+++ src/Search/Search.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Row, Col, Button } from 'antd';
+import styles from './Search.module.scss';
+
+const Search = () => {
+  const onSearchClick = () => {};
+
+  const onfilterClick = () => {};
+
+  return (
+    <section className={styles['app__search']}>
+      <Row type="flex" justify="space-between" align="middle">
+        <Col>
+          <Button type="link" icon="search">
+            Search
+          </Button>
+        </Col>
+
+        <Col>
+          <Button type="link" icon="filter">
+            Filter
+          </Button>
+        </Col>
+      </Row>
+    </section>
+  );
+};
+
+export default Search;
