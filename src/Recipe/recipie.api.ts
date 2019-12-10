1b80bf44bb977a95113bef0ee8fbbd38589b1650
Myles Gregory <hello@mylesgregory.me>
2019-12-10 02:25:46 -0800

    Minor changes to search api call

--- src/Recipe/recipie.api.ts
+++ src/Recipe/recipie.api.ts
@@ -12,23 +12,23 @@ export interface SearchParams {
 
 export const recipeApi = (params: SearchParams): Promise<Response> => {
   const paramsString = [
-    'apiKey=d13c1bf1dc21411db4c16f6c87dbc225',
-    params.query ? `query=${params.query}` : '',
-    params.cuisine ? `cuisine=${params.cuisine}` : '',
-    params.diet ? `diet=${params.diet}` : '',
+    "apiKey=d13c1bf1dc21411db4c16f6c87dbc225",
+    params.query ? `query=${params.query}` : "query=popular",
+    params.cuisine ? `cuisine=${params.cuisine}` : "",
+    params.diet ? `diet=${params.diet}` : "",
     params.excludeIngredients
       ? `excludeIngredients=${params.excludeIngredients}`
-      : '',
-    params.intolerances ? `intolerances=${params.intolerances}` : '',
-    params.offset ? `offset=${params.offset}` : '',
-    params.number ? `number=${params.number}` : '',
-    params.limitLicense ? `limitLicense=${params.limitLicense}` : '',
+      : "",
+    params.intolerances ? `intolerances=${params.intolerances}` : "",
+    params.offset ? `offset=${params.offset}` : "",
+    params.number ? `number=${params.number}` : "",
+    params.limitLicense ? `limitLicense=${params.limitLicense}` : "",
     params.instructionsRequired
       ? `instructionsRequired=${params.instructionsRequired}`
-      : ''
+      : ""
   ]
     .filter(p => p.length > 0)
-    .join('&');
+    .join("&");
 
   return fetch(`https://api.spoonacular.com/recipes/search?${paramsString}`);
 };
